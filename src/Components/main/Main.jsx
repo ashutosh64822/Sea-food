612338189ad98d4aed1b63cbccf8ee6118c62b54
ashutosh roy <ashutosh64822@gmail.com>
2025-08-22 22:28:36 +0600

    add orderd button

--- src/Components/main/Main.jsx
+++ src/Components/main/Main.jsx
@@ -6,9 +6,17 @@ const Main = ({ fetchData }) => {
 
     return (
         <>
-            <div className=' my-4'>
-                <h1 className='text-3xl font-semibold text-center '>Seafood Dishes</h1>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4'>
+            <div
+                className=' my-4'
+            >
+                <h1
+                    className='text-3xl font-semibold text-center '
+                >
+                    Seafood Dishes
+                </h1>
+                <div
+                    className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4'
+                >
                     {foods.meals.map(food => <Food key={food.idMeal} food={food} />)}
                 </div>
             </div>
